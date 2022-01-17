9dfef77301e568dad911131759a8516a45919277
yuan zhang <yuan.zhang2@yuans-MacBook-Pro.local>
2022-01-17 15:07:12 +0800

    启动项目初始化配置

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ const express = require('express');
 
 const app = express();
 
-const config = require('./config/dev.json');
 const routes = require('./routes/index');
 
+const envArgs = process.argv.splice(2);
+
+const config = envArgs[0] ? require('./config/dev.json') : require('./config/prod.json');
+
+console.log(config);
 app.use('/', routes);
 
 app.listen(config.port);
